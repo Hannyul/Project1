4d32241557cd924c47432758d09451813ae99412
avocado-toast99 <37838166+avocado-toast99@users.noreply.github.com>
2018-10-03 17:36:29 +0300

    Update LinkedList.java

--- LinkedList.java
+++ LinkedList.java
@@ -1,8 +1,8 @@
+
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+ADDDD NNAAAAAMMMMMEEEEEESSSS!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+*/
 package project1;
 
 /**
@@ -11,16 +11,20 @@ package project1;
  */
 public class LinkedList {
 
+    // declare
     private LLnode head;
     int neighbor;
     int length;
 
+    // create constructor
     public LinkedList() {
 
+        // initialize
         this.head = null;
         this.length = 0;
     }
 
+    // getters + setters
     public LLnode getHead() {
         return head;
     }
@@ -37,75 +41,64 @@ public class LinkedList {
         this.length = length;
     }
 
-    public boolean isEmpty() {
-        return head == null;
-    }
-
-    public int getNeighbors(int data) {
-        return getNeighbors(head, data);
-    }
-
-    private int getNeighbors(LLnode p, int data) {
-        LLnode helpPtr = p;
-        while (helpPtr != null) {
-            if (helpPtr.getData() == data) {
-                return helpPtr.getData();
-            }
-            helpPtr = helpPtr.getNext();
-        }
-        return 0;
-    }
-
+    // this method inserts a node to the list (unweighted graph) 
     public void insert(int data) {
         head = insert(head, data);
     }
 
     private LLnode insert(LLnode head, int data) {
 
+        // if the list is empty OR if the node value is less than the head
+        // we insert in the beginning
         if (head == null || head.getData() > data) {
             head = new LLnode(data, head);
             return head;
         } else {
 
+            // create a help pointer to traverse down the list
             LLnode helpPtr = head;
-
             while (helpPtr.getNext() != null) {
-
+                // if the value is less than the node's value, 
+                // then exit the while loop because we will insert at the end 
                 if (helpPtr.getNext().getData() > data) {
                     break;
                 }
 
                 helpPtr = helpPtr.getNext();
             }
-
+            // create new node and insert it at the end of the list
             LLnode newNode = new LLnode(data, helpPtr.getNext());
             helpPtr.setNext(newNode);
         }
         // Return head
         return head;
     }
-
+    // this method inserts a node and its weight to the list (weighted graph) 
     public void insertWeighted(int data, int weight) {
         head = insertWeighted(head, data, weight);
     }
 
     private LLnode insertWeighted(LLnode head, int data, int weight) {
 
+        // if the list is empty OR if the node value is less than the head
+        // we insert in the beginning
         if (head == null || head.getData() > data) {
             head = new LLnode(data, weight, head);
             return head;
         } else {
+            // create a help pointer to traverse down the list
 
             LLnode helpPtr = head;
 
             while (helpPtr.getNext() != null) {
-
+                // if the value is less than the node's value, 
+                // then exit the while loop because we will insert at the end 
                 if (helpPtr.getNext().getData() > data) {
                     break;
                 }
                 helpPtr = helpPtr.getNext();
             }
-
+               // create new node and insert it at the end of the list
             LLnode newNode = new LLnode(data, helpPtr.getNext());
             newNode.setWeight(weight);
             helpPtr.setNext(newNode);
@@ -114,6 +107,7 @@ public class LinkedList {
         return head;
     }
 
+    // this method prints the list
     public void PrintList() {
         PrintList(head);
     }
@@ -123,7 +117,7 @@ public class LinkedList {
         LLnode helpPtr = head;
 
         while (helpPtr != null) {
-
+            // print the data of the helpPtr
             System.out.print(helpPtr.getData() + " ");
             // Step one node over
             helpPtr = helpPtr.getNext();
@@ -132,8 +126,5 @@ public class LinkedList {
 
     }
 
-    public void delete() {
-        head = null;
-    }
 
 }// class
